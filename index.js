4a683eed0fd92f26acb5f33c22757c248f1729fa
Christian Brevik <christian.brevik@gmail.com>
2018-09-21 14:49:27 +0200

    Add optional array-reverse-polyfill in comment

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 /** @format */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// import { Platform } from "react-native";
+// if (parseInt(Platform.Version, 10) === 12) {
+//   require("array-reverse-polyfill");
+// }
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
 
 AppRegistry.registerComponent(appName, () => App);
